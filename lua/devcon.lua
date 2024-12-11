84368669bf80f2a718e45b8ddfcb23d7d20adf8d
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2024-12-11 16:12:02 +0100

    Add CLI (docker/podman) setting to setup()

--- lua/devcon.lua
+++ lua/devcon.lua
@@ -1,11 +1,26 @@
 local M = {}
 
-M.setup = function()
-	print("DevCon setup!")
+M.setup = function(opts)
+	opts = opts or {}
+
+	M.settings = {}
+
+	-- Set or detect CLI
+	M.settings.cli = opts.cli
+	if not M.settings.cli then
+		M.settings.cli = 'docker'
+		local podman_v = io.popen("podman -v 2>/dev/null")
+		if podman_v:read(1) then
+			M.settings.cli = 'podman'
+		end
+	end
+
 end
 
 M.devcon = function()
-	print("DevCon!")
+	if not M.settings then
+		print("Call require('devcon').setup() first.")
+	end
 end
 vim.api.nvim_create_user_command('DevCon', M.devcon, {})
 
